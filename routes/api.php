8791d51d07cf3fc829da6a10c6b4de26c921be96
sashokrist <alexander.krist@gmail.com>
2025-03-22 11:13:18 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\BookingController;
 use App\Http\Controllers\API\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -7,5 +9,10 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('bookings', \App\Http\Controllers\API\BookingController::class);
+
+    Route::get('/bookings', [BookingController::class, 'index']);
+    Route::post('/bookings', [BookingController::class, 'store']);
+    Route::get('/bookings/{booking}', [BookingController::class, 'show']);
+    Route::put('/bookings/{booking}', [BookingController::class, 'update']);
+    Route::delete('/bookings/{booking}', [BookingController::class, 'destroy']);
 });
